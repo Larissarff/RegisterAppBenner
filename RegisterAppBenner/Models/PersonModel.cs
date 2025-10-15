71969f60bf6650889bdbaeeafc788a950f799b81
Larissarff <larissa.rfferreira4800@gmail.com>
2025-10-15 15:49:00 -0300

    refactor: refactoring models and project solution

--- RegisterAppBenner/Models/PersonModel.cs
+++ RegisterAppBenner/Models/PersonModel.cs
@@ -1,12 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RegisterAppBenner.Models
 {
-    internal class Person
+    public class PersonModel
     {
+        private static int _nextId = 1;
+        public int Id { get; private set; }
+        public string Name { get; set; } = string.Empty;
+        public string Cpf { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+
+        public PersonModel(string name, string cpf, string address = "")
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Name is required.", nameof(name));
+            if (!IsValidCpf(cpf))
+                throw new ArgumentException("Invalid CPF.", nameof(cpf));
+
+            Id = _nextId++;
+            Name = name.Trim();
+            Cpf = cpf.Trim();
+            Address = address.Trim();
+        }
+
+        private bool IsValidCpf(string cpf)
+        {
+            if (string.IsNullOrWhiteSpace(cpf))
+                return false;
+            if (cpf.Length != 11 || !cpf.All(char.IsDigit))
+                return false;
+
+            return true; 
+        }
     }
 }
--- RegisterAppBenner/Models/ProductModel.cs
+++ RegisterAppBenner/Models/ProductModel.cs
@@ -1,12 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RegisterAppBenner.Models
 {
-    internal class ProductModel
+    public class ProductModel
     {
+        private static int _nextId = 1;
+        public int Id { get; private set; }
+        public string Name { get; set; } = string.Empty;
+        public string Code { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+
+        public ProductModel( string name, string code, decimal price)
+        {
+            Id = _nextId++;
+
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Name is required.", nameof(name));
+
+            if (!int.TryParse(code, out int parsedCode))
+                throw new ArgumentException("Invalid Code.", nameof(code));
+
+            if (price < 0)
+                throw new ArgumentException("Price cannot be negative.", nameof(price));
+
+            Name = name.Trim();
+            Code = code.Trim();
+            Price = price;
+        }
+
     }
 }
