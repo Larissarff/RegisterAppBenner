3d39a2d7bbad6d967753e9593db38f01f03133c8
Larissarff <larissa.rfferreira4800@gmail.com>
2025-10-16 18:41:23 -0300

    feat: new screen to make orders

---
+++ RegisterAppBenner/Views/OrderView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using RegisterAppBenner.ViewModels;
+
+namespace RegisterAppBenner.Views
+{
+    public partial class OrderView : UserControl 
+    {
+        private readonly OrderViewModel _viewModel;
+
+        public OrderView()
+        {
+            InitializeComponent();
+            _viewModel = (OrderViewModel)DataContext;
+        }
+
+        private void AddItem_Click(object sender, RoutedEventArgs e)
+        {
+            _viewModel.AddItemToOrder();
+        }
+
+        private void FinalizeOrder_Click(object sender, RoutedEventArgs e)
+        {
+            _viewModel.FinalizeOrder();
+        }
+    }
+}
